ef07b3abe3cf4c45e639fe76a63d34300c8c5f6d
Moutamid <lumianokia188@gmail.com>
2022-10-20 23:54:10 +0500

    initial commit

--- app/src/main/java/com/moutamid/simplelogin/MainActivity.java
+++ app/src/main/java/com/moutamid/simplelogin/MainActivity.java
@@ -3,6 +3,11 @@ package com.moutamid.simplelogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        Glide.with(this)
+                .load("https://source.unsplash.com/random")
+                .apply(new RequestOptions()
+                        .placeholder(R.color.grey)
+                        .error(R.color.red)
+                )
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(imageView);
     }
 }
